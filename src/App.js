05e003513ca221147cb776bd0d428a2a74643c16
sfrancop <s.francop@uniandes.edu.co>
2023-10-06 21:30:15 -0500

    gh pages y react router funcionando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Inicio from "./pages/Inicio"
+import Sistema from "./pages/Sistema"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <HashRouter >
+        <Routes>
+          <Route path="/" element={<Inicio/>} />
+          <Route path="/sistema" element={<Sistema/>} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
